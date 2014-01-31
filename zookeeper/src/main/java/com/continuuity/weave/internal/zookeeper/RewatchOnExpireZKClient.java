c28b5ad3101c2b4a32a99b83fc570ff0e4ada522
Terence Yim <terence@continuuity.com>
2014-01-31 12:31:10 -0800

    Check for null watcher and not to add a rewatchWatch if it is null.

--- zookeeper/src/main/java/com/continuuity/weave/internal/zookeeper/RewatchOnExpireZKClient.java
+++ zookeeper/src/main/java/com/continuuity/weave/internal/zookeeper/RewatchOnExpireZKClient.java
@@ -38,6 +38,9 @@ public final class RewatchOnExpireZKClient extends ForwardingZKClient {
 
   @Override
   public OperationFuture<Stat> exists(String path, Watcher watcher) {
+    if (watcher == null) {
+      return super.exists(path, watcher);
+    }
     final RewatchOnExpireWatcher wrappedWatcher = new RewatchOnExpireWatcher(this, ActionType.EXISTS, path, watcher);
     OperationFuture<Stat> result = super.exists(path, wrappedWatcher);
     Futures.addCallback(result, new FutureCallback<Stat>() {
@@ -56,6 +59,9 @@ public final class RewatchOnExpireZKClient extends ForwardingZKClient {
 
   @Override
   public OperationFuture<NodeChildren> getChildren(String path, Watcher watcher) {
+    if (watcher == null) {
+      return super.getChildren(path, watcher);
+    }
     final RewatchOnExpireWatcher wrappedWatcher = new RewatchOnExpireWatcher(this, ActionType.CHILDREN, path, watcher);
     OperationFuture<NodeChildren> result = super.getChildren(path, wrappedWatcher);
     Futures.addCallback(result, new FutureCallback<NodeChildren>() {
@@ -74,6 +80,9 @@ public final class RewatchOnExpireZKClient extends ForwardingZKClient {
 
   @Override
   public OperationFuture<NodeData> getData(String path, Watcher watcher) {
+    if (watcher == null) {
+      return super.getData(path, watcher);
+    }
     final RewatchOnExpireWatcher wrappedWatcher = new RewatchOnExpireWatcher(this, ActionType.DATA, path, watcher);
     OperationFuture<NodeData> result = super.getData(path, wrappedWatcher);
     Futures.addCallback(result, new FutureCallback<NodeData>() {
